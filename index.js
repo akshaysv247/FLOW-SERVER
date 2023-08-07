1109200c99ec0a56a0378645a064c4161505d6e1
nelson <akshaysv957@gmail.com>
2023-08-07 16:48:17 +0530

    deployment has changed

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ const app = express();
 // database
 Database();
 
+// app.use(
+//   cors({
+//     origin: ['https://flowmusics.online'],
+//     // origin: ['http://localhost:5173'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//   }),
+// );
 app.use(
   cors({
     origin: ['https://flowmusics.online'],
-    // origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
